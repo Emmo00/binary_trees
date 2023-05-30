837c1ae7db71c9acb6a1b5261ed95ed0881bf447
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-30 22:13:34 +0100

    is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - checks if a node is a root node
+ * @node: node to check
+ * Return: 1 if node is a root node else 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (!node->parent)
+		return (1);
+	return (0);
+}
