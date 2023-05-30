a6f6f27c49289ba22ecd7c38db5c0306f79998fa
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-30 19:22:11 +0100

    right

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,6 +16,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
-	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
