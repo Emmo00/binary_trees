a162017fc1001dab3b2afae1bafa2c794a2d9e81
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-31 23:36:18 +0100

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,9 +21,17 @@ int height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int height_l, height_r;
+
 	if (tree == NULL)
 		return (0);
-	if (height(tree->left) >= height(tree->right))
+	height_l = height(tree->left);
+	height_r = height(tree->right);
+	if (height_l >= height_r)
+	{
+		if (height_l > height_r)
+			return (binary_tree_is_perfect(tree->left));
 		return (1);
+	}
 	return (0);
 }
