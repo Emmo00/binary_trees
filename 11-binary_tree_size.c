f2e1a3fa52ef4c0b7a27343aa768d4d19e2e2a10
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-31 13:46:46 +0100

    size2

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,13 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_size - finds the size of a binary tree
+ * @tree: pointer to root node of the binary tree
+ * Return: size of the binary tree, returns 0 if tree is NULL
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size;
+
 	if (tree == NULL)
 		return (0);
 	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
