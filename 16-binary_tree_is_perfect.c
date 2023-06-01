109282139564fd2748ec02a06e2a6f09d04dfa2d
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-01 21:49:19 +0100

    ls

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
 #include "binary_trees.h"
-/**
- * height - finds the height of a binary subtree
- * @tree: root node of the binary tree
- * Return: height of the subtree
- */
-int height(const binary_tree_t *tree)
-{
-	int height_left, height_right;
-
-	if (tree == NULL)
-		return (-1);
-	height_left = (tree->left) ? 1 + height(tree->left) : 0;
-	height_right = (tree->right) ? 1 + height(tree->right) : 0;
-	return ((height_left > height_right) ? height_left : height_right);
-}
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the binary tree
@@ -22,20 +6,20 @@ int height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height_l, height_r;
-
-	if (!tree->left && !tree->right)
-		return (1);
 	if (tree == NULL)
-		return (0);
-	height_l = height(tree->left);
-	height_r = height(tree->right);
-	if (height_l < height_r)
 		return (0);
 	if (tree->left && tree->right)
 		return (binary_tree_is_perfect(tree->left) &
 				binary_tree_is_perfect(tree->right));
-	if (tree->left)
-		return (binary_tree_is_perfect(tree->left));
+	if (!tree->left && !tree->right)
+	{
+		binary_tree_t *parent;
+
+		parent = tree->parent;
+		if (tree == parent->left)
+			if (parent->right->left || parent->right->right)
+				return (0);
+		return (1);
+	}
 	return (0);
 }
